7783ef9434fcff862bb3d5b1f6c952c72533073e
Enso Vergara Gutierrez <ensovergara@gmail.com>
2020-05-14 15:39:53 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# yeyox09.github.io
-página personal con información de CV.
---
+++ precache-manifest.411fb5d6f04d9bafa63a75729fcef3e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff44e51372f83cc6adcb54fb4a9387b",
+    "url": "/portafolio-react/index.html"
+  },
+  {
+    "revision": "92b32f2faee552fb0544",
+    "url": "/portafolio-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "794d273e0b573922bf77",
+    "url": "/portafolio-react/static/js/2.20bd1c49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio-react/static/js/2.20bd1c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b32f2faee552fb0544",
+    "url": "/portafolio-react/static/js/main.18e0a060.chunk.js"
+  },
+  {
+    "revision": "548738aa081af57d2ed6",
+    "url": "/portafolio-react/static/js/runtime-main.5771537b.js"
+  }
+]);
